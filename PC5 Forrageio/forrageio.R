5b09a3e806984382205a12cbea0eac2991b50198
Marco Mello <marmello@usp.br>
2022-05-04 09:48:17 -0300

    PC5 additional analysis

--- PC5 Forrageio/forrageio.R
+++ PC5 Forrageio/forrageio.R
@@ -87,3 +87,35 @@ resultados5 = summary(fit2)
 resultados5
 
 capture.output(resultados5, file = "resultados/res5.txt")
+
+
+######################### FIGURA 2 DO ARTIGO ###################################
+
+
+p2 = ggplot(dados, aes(x=corcas, y=tipopresa)) + 
+  geom_point(colour = "blue", size=4, alpha = 0.3) + 
+  stat_smooth(method="glm", method.args=list(family="binomial"),
+              se=T, colour = "blue", fill = "blue", alpha = 0.1) +
+  labs(x="Roe deer density [km-2]", y = "P(roe deer vs. moose kill)") +
+  theme(text = element_text(size=20),
+        plot.title = element_text(size=40, hjust=0.5),
+        axis.text.x = element_text(size = 20, angle=0, hjust=1),
+        axis.text.y = element_text(size = 20, angle=0, vjust=1),
+        axis.title.x = element_text(size = 30, angle=0),
+        axis.title.y = element_text(size = 30, angle=90)) +
+  scale_x_continuous(breaks = seq(0, 10, by = 2)) +
+  scale_y_continuous(breaks = seq(0, 1, by = 0.5))
+
+p2 
+
+png(filename= "figuras/p2.png", res= 300, height= 3000, width= 3500)
+p2
+dev.off()
+
+
+fit3 = glm(tipopresa~corcas, family=binomial(link="logit")) 
+
+resultados6 = summary(fit3)
+resultados6
+
+capture.output(resultados6, file = "resultados/res6.txt")
