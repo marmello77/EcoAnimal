358ec6474f24cff19db5515b2c4f0771923189be
Marco Mello <marmello@usp.br>
2022-05-10 14:28:32 -0300

    Create socialidade.R

---
+++ PC3 Socialidade/socialidade.R
@@ -0,0 +1,19 @@
+################################################################################
+# Universidade de São Paulo
+# Instituto de Biociências
+# Departamento de Ecologia
+# Topicos Avançados em Ecologia de Animais (BIE0315)
+# Profs. José Carlos Motta Jr. & Marco A. R. Mello
+# Prática de Computador III: Socialidade
+# README: https://github.com/marmello77/EcoAnimal#readme
+################################################################################
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+rm(list= ls())
+
+cat("\014")  
+
+library(RNetLogo)
+
+############################### EM CONSTRUÇÃO ##################################
