48683a1389bd93a468c43a067efe7dc194757381
Marco Mello <marmello@usp.br>
2022-05-31 06:19:45 -0300

    PC3 completada
    PC3 com script e tutorial.

--- PC3 Socialidade/socialidade.R
+++ PC3 Socialidade/socialidade.R
@@ -17,10 +17,9 @@ cat("\014")
 library(igraph)
 library(deSolve)
 library(rJava)
-library(RNetLogo)
 
 
-######################### MODELO SIR SIMPLES ###################################
+########################## MODELO SIR BÁSICO ###################################
 
 
 # Crie uma função SIR
@@ -62,9 +61,13 @@ p1 <- matplot(x = times, y = out, type = "l",
         xlab = "Tempo", 
         ylab = "Suscetíveis, Infectados e Recuperados",
         main = "Modelo SIR",
-        lwd = 1, lty = 1, bty = "l", col = 2:4)
+        lwd = 1, lty = 1, bty = "l",
+        col = c("blue", "red", "grey"))
 
-legend(80, 0.9, c("Susceptible", "Infected", "Recovered"), pch = 1, col = 2:4, bty = "n")
+legend(80, 0.9, c("Suscetíveis", "Infectados", "Recuperados"),
+       pch = 1, 
+       col = c("blue", "red", "grey"), 
+       bty = "n")
 
 p1
 
@@ -78,35 +81,26 @@ rm(list= ls())
 
 # Crie uma rede com estrutura de mundo pequeno (aka "small world" ou
 # Watts-Strogatz)
-g <- sample_smallworld(1,         #tamanho inicial da rede
-                       43,        #tamanho final da rede
-                       6,        #grau médio
-                       0.05,      #probabilidade de reconexão
-                       loops = F) #a rede pode ter loops ou não?
+g <- sample_smallworld(dim = 1,      #tamanho inicial da rede
+                       size = 43,    #tamanho final da rede
+                       nei = 12,     #grau médio
+                       p = 0.05,     #probabilidade de reconexão
+                       loops = F,    #a rede pode ter loops ou não?
+                       multiple = F) #pode haver elos múltiplos ou não?
+
 plot(g)
 
 
-sm <- sir(g,           #a rede que você criou
-          beta=1.025,  #probabilibidade de infecção
-          gamma=0.2, #probabilidade de recuperação
-          no.sim = 1)  #número de rodadas de simulação
+sm <- sir(g,            #a rede que você criou
+          beta = 0.02,  #probabilibidade de infecção
+          gamma = 0.02, #probabilidade de recuperação
+          no.sim = 1)   #número de rodadas de simulação
 
-plot(sm[[1]]$NS~sm[[1]]$times, col = "blue",
+plot(sm[[1]]$NS~sm[[1]]$times,
+     col = "blue",
      type = "l",
      xlab = "Tempo",
      ylab = "Suscetíveis, Infectados e Recuperados")
 lines(sm[[1]]$NI~sm[[1]]$times,col="red")
 lines(sm[[1]]$NR~sm[[1]]$times,col="grey")
 
-
-
-######################### FONTES CONSULTADAS ###################################
-
-
-#1. http://rstudio-pubs-static.s3.amazonaws.com/6852_c59c5a2e8ea3456abbeb017185de603e.html
-
-#2. https://archives.aidanfindlater.com/blog/2010/04/20/the-basic-sir-model-in-r/ 
-
-#3. https://igraph.org/c/doc/igraph-Generators.html
-
-#4. https://rdrr.io/cran/igraph/man/sir.html
